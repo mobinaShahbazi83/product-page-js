a8d0504a133a57910080e0ee455700c24ae80d6e
mobinaShahbazi83 <shzmobina249@gmail.com>
2025-03-09 12:35:16 +0330

    Filter products by price

--- js/app.js
+++ js/app.js
@@ -42,7 +42,17 @@ const filterHandler = (event) => {
 }
 
 const searchPriceHandler = (event) => {
-    const searchPrice = event.target.parentElement.children[0].value
+    const searchPrice = +event.target.parentElement.children[0].value
+    products.forEach((product) => {
+        const productPrice = product.children[2].innerText
+        const price = +productPrice.split(" ")[1]
+        if(!searchPrice) {
+               product.style.display = "block"
+        } else {
+            searchPrice === price ? (product.style.display = "block") : ( product.style.display = "none")
+        }
+    })
+
 }
 searchInput.addEventListener("keyup", searchHandler)
 
